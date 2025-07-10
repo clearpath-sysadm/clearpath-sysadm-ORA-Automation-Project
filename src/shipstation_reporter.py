5d62ef1550eb1ed513244ac1659ecf8207ad7786
Nathan Neely <me@n8muzik.com>
2025-07-10 15:54:58 -0500

    Trigger Cloud Build for initial function deployment

--- src/shipstation_reporter.py
+++ src/shipstation_reporter.py
@@ -1,5 +1,6 @@
 # TESTING DEPLOYMENT 2025-07-10 (2ND ATTEMPT)#
 # 3RD ATTEMPT#
+# 4RD ATTEMPT#
 
 import pandas as pd
 from datetime import datetime, date # Import date as well for explicit date objects
